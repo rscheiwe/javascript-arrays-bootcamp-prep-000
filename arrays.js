1a5066dcecd2355bef4fae91a94a87d87357bc78
Richard S <rscheiwe@gmail.com>
2018-03-18 04:16:34 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -30,7 +30,6 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 }
 
 function removeElementFromBeginningOfArray(array) {
-  array.slice(1)
-  return array
+  return array.slice(1)
 }
 
