d356f2ae422e0fa047efd65318cbf9808b33970e
Richard S <rscheiwe@gmail.com>
2018-03-18 04:14:01 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -7,7 +7,8 @@ function addElementToBeginningOfArray (array, element) {
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  return array.unshift(element)
+  array.unshift(element)
+  return array
 }
 
 function addElementToEndOfArray(array, element) {
@@ -15,7 +16,8 @@ function addElementToEndOfArray(array, element) {
 }
 
 function destructivelyAddElementToEndOfArray(array, element) {
-  return array.push(element)
+  array.push(element)
+  return array
 }
 
 function accessElementInArray(array, index) {
